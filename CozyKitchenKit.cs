7b16fb45fd9409b02ede3b9e7a3b6e99d5fa17c1
RedGindew <redgindew@gmail.com>
2025-01-11 18:09:18 +0000

    1.5 Release

--- CozyKitchenKit.cs
+++ CozyKitchenKit.cs
@@ -31,9 +31,9 @@ public class CozyKitchenKit : Mod
 
     // visual data about this mod
     public override string Name => "Cozy Kitchen Kit";
-    public override string Description => "Live your cottagecore dreams! - By Gindew v1.3";
+    public override string Description => "Live your cottagecore dreams!";
     public override string IssueTrackerUrl => "https://x.com/RedGindew";
-    public override string TestedVersionRange => "[0.45.0]";
+    public override string TestedVersionRange => "[0.46.0]";
     private Dictionary<Point, TextureRegion> uiTextures;
     public override TextureRegion Icon => this.uiTextures[new Point(0, 0)];
 
@@ -43,12 +43,12 @@ public class CozyKitchenKit : Mod
     public override void Initialize(Logger logger, RawContentManager content, RuntimeTexturePacker texturePacker, ModInfo info)
     {
         CozyKitchenKit.Logger = logger;
-        texturePacker.Add(new UniformTextureAtlas(content.Load<Texture2D>("UITex"), 8, 8), r => this.uiTextures = r, 1, true);
+        texturePacker.Add(new UniformTextureAtlas(content.Load<Texture2D>("UITex"), 8, 8), r => this.uiTextures = r, 1, true, true);
     }
 
     public override void AddGameContent(GameImpl game, ModInfo info)
     {
-        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyCounter", new Point(1, 1), ObjectCategory.Counter, 80, new ColorScheme[] { ColorScheme.SimpleWood, ColorScheme.SimpleWood, ColorScheme.White })
+        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyCounter", new Point(1, 1), ObjectCategory.Counter, 80)
         {
             Icon = this.Icon,
             Tab = (FurnitureTool.Tab.Kitchen),
@@ -57,43 +57,41 @@ public class CozyKitchenKit : Mod
             Colors = new ColorSettings(ColorScheme.SimpleWood, ColorScheme.SimpleWood, ColorScheme.White) { Defaults = new int[] { 1, 0, 0 }, PreviewName = "CozyKitchenKit.CozyCounter" },
             ObjectSpots = ObjectSpot.CounterSpots(false).ToArray()
         });
-        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyFridge", new Point(1, 1), ObjectCategory.Fridge, 800, new ColorScheme[] { ColorScheme.MutedPastels, ColorScheme.White })
+        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyFridge", new Point(1, 1), ObjectCategory.Fridge, 800)
         {
             Icon = this.Icon,
             Tab = (FurnitureTool.Tab.Kitchen),
             Colors = new ColorSettings(ColorScheme.MutedPastels, ColorScheme.White){ Defaults = new int[] { 3, 0 }},
             DefaultRotation = MLEM.Maths.Direction2.Right
         });
-                FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyOven", new Point(1, 1), ObjectCategory.Stove | ObjectCategory.Oven, 600)
+        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyOven", new Point(1, 1), ObjectCategory.Stove | ObjectCategory.Oven, 600)
         {
             Icon = this.Icon,
-            Reliability = 5,
             Tab = (FurnitureTool.Tab.Kitchen),
             ActionSpots = new[] { new ActionSpot(Vector2.Zero, MLEM.Maths.Direction2.Up) },
             Colors = new ColorSettings(ColorScheme.MutedPastels, ColorScheme.White){ Defaults = new int[] { 3, 0 }, Map = new int[] {0, 0, 1, 1} },
             ObjectSpots = ObjectSpot.CounterSpots(true),
             DefaultRotation = MLEM.Maths.Direction2.Right,
-            ElectricityRating = 2
+            ElectricityRating = 2,
         });
-        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyShelf", new Point(1, 1), ObjectCategory.NonColliding | ObjectCategory.WallHanging, 800, new ColorScheme[] { ColorScheme.SimpleWood, ColorScheme.White })
+        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyShelf", new Point(1, 1), ObjectCategory.NonColliding | ObjectCategory.WallHanging, 800)
         {
             Icon = this.Icon,
             Tab = (FurnitureTool.Tab.Kitchen),
             Colors = new ColorSettings(ColorScheme.SimpleWood, ColorScheme.White){ Defaults = new int[] { 1, 0 }, PreviewName = "CozyKitchenKit.CozyShelf" },
             DefaultRotation = MLEM.Maths.Direction2.Right
         });
-        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozySink", new Point(1, 1), ObjectCategory.CounterObject | ObjectCategory.DisallowedOnGround | ObjectCategory.Sink, 350, new ColorScheme[] { ColorScheme.MutedPastels, ColorScheme.White })
+        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozySink", new Point(1, 1), ObjectCategory.CounterObject | ObjectCategory.DisallowedOnGround | ObjectCategory.Sink, 350)
         {
             Icon = this.Icon,
             Tab = (FurnitureTool.Tab.Kitchen),
             Colors = new ColorSettings(ColorScheme.MutedPastels, ColorScheme.White){Defaults = new int[] { 3, 0 } },
-            Reliability = 5,
             ActionSpots = new[] { new ActionSpot(Vector2.Zero, MLEM.Maths.Direction2.Up) },
             ObjectSpots = ObjectSpot.CounterSpots(true),
             DefaultRotation = MLEM.Maths.Direction2.Right,
             ElectricityRating = 2
         });
-        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyCabinet", new Point(1, 1), ObjectCategory.Nothing, 800, new ColorScheme[] { ColorScheme.SimpleWood, ColorScheme.White })
+        FurnitureType.Register(new FurnitureType.TypeSettings("CozyKitchenKit.CozyCabinet", new Point(1, 1), ObjectCategory.Nothing, 800)
         {
             Icon = this.Icon,
             Tab = (FurnitureTool.Tab.Kitchen),
